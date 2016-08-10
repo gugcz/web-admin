933f57964a331cd46d22d96a6ebeaa950b66d6fd
HormCZ <horak@horm.cz>
2016-08-10 10:42:52 +0200

    Add route

--- src/app/app.js
+++ src/app/app.js
@@ -1,14 +1,87 @@
 (function() {
   'use strict';
 
+    function AddChapterCtrl() {
+        var self = this;
+        var pendingSearch, cancelSearch = angular.noop;
+        var cachedQuery, lastSearch;
+        // var organizers = loadContactsProfilePicture(firebaseData.getAllOrganizers());
+        var organizers = [];
+
+        self.asyncContacts = [];
+        self.filterSelected = true;
+
+        self.querySearch = function (criteria) {
+            cachedQuery = cachedQuery || criteria;
+            return cachedQuery ? organizers.filter(createFilterFor(cachedQuery)) : [];
+        };
+
+        self.delayedQuerySearch = function (criteria) {
+            cachedQuery = criteria;
+            if ( !pendingSearch || !debounceSearch() )  {
+                cancelSearch();
+
+                // http get
+                return pendingSearch = $q(function (resolve, reject) {
+
+                    cancelSearch = reject;
+                    // Now, it shows all contacts
+                    refreshDebounce();
+                    resolve(self.querySearch());
+                })
+            }
+
+            return pendingSearch;
+        };
+
+        self.chapter = {
+            section: 'GDG',
+            name: '',
+            description: '',
+            profilePicture: '',
+            orgs: [],
+            email: '',
+            googlePlusLink: '',
+            facebookLink: '',
+            twitterLink: '',
+            meetupID: '',
+            meetupURL: '',
+            coordinates: ''
+
+        };
+
+        function refreshDebounce() {
+            lastSearch = 0;
+            pendingSearch = null;
+            cancelSearch = angular.noop;
+        }
+
+        function debounceSearch() {
+            var now = new Date().getMilliseconds();
+            lastSearch = lastSearch || now;
+
+            return ((now - lastSearch) < 300);
+        }
+
+        function loadContactsProfilePicture(organizers) {
+
+            return organizers.map(function (org, index) {
+                var organizer = {
+                    name: org.name,
+                    email: org.mail,
+                    image: gravatar(org.mail)
+                };
+                return organizer;
+            });
+        }
+    }
+
   angular.module('gugCZ.webAdmin', [
         'angular-loading-bar',
         'ngMaterial',
         'ui.router',
         'pascalprecht.translate',
-
         'gugCZ.auth',
-
         'gugCZ.webAdmin.templates',      // templates in template cache
         'gugCZ.webAdmin.translations',  // translations from locale files
 
@@ -42,7 +115,13 @@
           },
           controllerAs: 'app',
           templateUrl: 'app/app.html'
-        });
+        })
+            .state('chapter-add', {
+                url: '/chapter/add',
+                templateUrl: 'app/chapter/add/add.html',
+                controller: AddChapterCtrl,
+                controllerAs: 'vm'
+            });
       })
 
       .config(function($urlRouterProvider) {
--- src/app/chapter/add/app.js
+++ src/app/chapter/add/app.js
@@ -2,7 +2,9 @@
  * Created by horm on 1.8.16.
  */
 'use strict';
-angular.module('AddChapterApp', ['ngMaterial', 'ngMessages'])
+angular.module('AddChapterApp', ['ngMaterial', 'ngMessages', 'FIREBASE_URL'])
+    .constant('FIREBASE_URL', 'https://helloworld-ee64c.firebaseio.com/')// Testing project for
+    .service('firebaseData', ['firebase', 'FIREBASE_URL', FirebaseFactory])
     .controller('AddChapterCtrl', AddChapterCtrl)
     .config(function ($mdThemingProvider) {
         // Configure a dark theme with primary foreground yellow
@@ -11,19 +13,18 @@ angular.module('AddChapterApp', ['ngMaterial', 'ngMessages'])
             .dark();
     });
 
-function AddChapterCtrl($q, $timeout) {
+function AddChapterCtrl(firebaseData) {
     var self = this;
     var pendingSearch, cancelSearch = angular.noop;
     var cachedQuery, lastSearch;
+    var organizers = loadContactsProfilePicture(firebaseData.getAllOrganizers());
 
-    self.allContacts = loadContacts();
-    self.contacts = [self.allContacts[0]];
     self.asyncContacts = [];
     self.filterSelected = true;
 
     self.querySearch = function (criteria) {
         cachedQuery = cachedQuery || criteria;
-        return cachedQuery ? self.allContacts.filter(createFilterFor(cachedQuery)) : [];
+        return cachedQuery ? organizers.filter(createFilterFor(cachedQuery)) : [];
     };
 
     self.delayedQuerySearch = function (criteria) {
@@ -32,16 +33,13 @@ function AddChapterCtrl($q, $timeout) {
             cancelSearch();
 
             // http get
-            return pendingSearch = $q(function(resolve, reject) {
-                // Simulate async search... (after debouncing)
+            return pendingSearch = $q(function (resolve, reject) {
+
                 cancelSearch = reject;
-                $timeout(function() {
-
-                    resolve( self.querySearch() );
-
-                    refreshDebounce();
-                }, Math.random() * 500, true)
-            });
+                // Now, it shows all contacts
+                refreshDebounce();
+                resolve(self.querySearch());
+            })
         }
 
         return pendingSearch;
@@ -63,10 +61,6 @@ function AddChapterCtrl($q, $timeout) {
 
     };
 
-    self.add = function () {
-        
-    };
-
     function refreshDebounce() {
         lastSearch = 0;
         pendingSearch = null;
@@ -80,39 +74,49 @@ function AddChapterCtrl($q, $timeout) {
         return ((now - lastSearch) < 300);
     }
 
-    function createFilterFor(query) {
-        var lowercaseQuery = angular.lowercase(query);
+    function loadContactsProfilePicture(organizers) {
 
-        return function filterFn(contact) {
-            return (contact._lowername.indexOf(lowercaseQuery) != -1);;
-        };
-
-    }
-
-    function loadContacts() {
-        var contacts = [
-            'Marina Augustine',
-            'Oddr Sarno',
-            'Nick Giannopoulos',
-            'Narayana Garner',
-            'Anita Gros',
-            'Megan Smith',
-            'Tsvetko Metzger',
-            'Hector Simek',
-            'Some-guy withalongalastaname'
-        ];
-
-        return contacts.map(function (c, index) {
-            var cParts = c.split(' ');
-            var contact = {
-                name: c,
-                email: cParts[0][0].toLowerCase() + '.' + cParts[1].toLowerCase() + '@example.com',
-                image: gravatar('example@example.com')
+        return organizers.map(function (org, index) {
+            var organizer = {
+                name: org.name,
+                email: org.mail,
+                image: gravatar(org.mail)
             };
-            contact._lowername = contact.name.toLowerCase();
-            return contact;
+            return organizer;
         });
     }
+}
+
+function FirebaseFactory(FIREBASE_URL, $firebaseArray) {
+    var self = this;
+    self.chapters = null;
+
+    self.getAllOrganizers = function () {
+        var ref = new Firebase(FIREBASE_URL  + '/orgs');
+        var organizers = $firebaseArray(ref);
+        return organizers;
+    };
+
+    self.addChapter = function (chapter) {
+        var ref = new Firebase(FIREBASE_URL  + '/chapters');
+        var chapters = $firebaseArray(ref);
+        chapters.$add(chapter)
+    };
+
+    self.addChapterWithOwnID = function (chapter) {
+        var chaptersRef = new Firebase(FIREBASE_URL + 'chapters/');
+        chaptersRef.child(getChapterID(chapter)).set(self.chapter);
+    };
+
+    self.getOrganizersByQuery = function (name) {
+
+    };
+
+    function getChapterID(chapter) {
+        return chapter.section.toLowerCase() + '_' + chapter.name.replace(' ', '_').toLowerCase();
+    }
+
+
 }
 
 
