494859d750d63fe977004f1bbfb5600f6b168485
Horm <horak@horm.cz>
2016-12-10 21:24:38 +0100

    Add next inputs (description, chapters) to event form

--- src/app/events/form/form.js
+++ src/app/events/form/form.js
@@ -2,12 +2,20 @@
 	'use strict';
 
 	function EventFormController(firebaseEvents) {
+
+		// TODO
 		var getNextMonthDate = function () {
 			return new Date();
 		};
+
+		// TODO prefill
 		this.event = {
 			name: 'GDG Coding Dojo Brno',
-			startDate: getNextMonthDate()
+			subtitle: 'Intenzivní trénink programátora',
+			startDate: getNextMonthDate(),
+			description: 'Tady bude popis',
+			chapters: ['GDG Brno']
+
 		}
 		this.addEvent = function () {
 			firebaseEvents.addEvent(this.event);
