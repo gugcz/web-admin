1aba6b748cab038fdaa8b7339493320b9ffed530
Milan Lempera <milanlempera@gmail.com>
2016-12-17 21:02:21 +0100

    event form date - start, end & duration

--- src/app/events/form/dates/dates-component.js
+++ src/app/events/form/dates/dates-component.js
@@ -1,59 +1,104 @@
-(function () {
-	'use strict';
+(function() {
+  'use strict';
 
-	var component = {
-		templateUrl: 'app/events/form/dates/dates.html',
-		controller: DatesController,
-		controllerAs: 'vm'
-	};
+  var component = {
+    templateUrl: 'app/events/form/dates/dates.html',
+    controller: DatesController,
+    controllerAs: 'vm'
+  };
 
-	function DatesController() {
-		this.dates = {
-			start: new Date(), // TODO Next month
-			end: new Date() // TODO after two hours
-		};
+  /**
+   * @param {Date} date
+   * @return {Date}
+   */
+  var getNextMonthDate = function(date) {
+    date = date || new Date();
 
-		this.isMultiDay = true;
+    if (date.getMonth() == 11) {
+      return new Date(date.getFullYear() + 1, 0, 1, 12);
+    } else {
+      return new Date(date.getFullYear(), date.getMonth() + 1, 1, 12);
+    }
 
-		this.duration = 120;
+  };
 
-    this.options = {
-      defaultDate: "2015-08-06",
-      minDate: "2015-01-01",
-      maxDate: "2015-12-31",
-      disabledDates: [
-        "2015-06-22",
-        "2015-07-27",
-        "2015-08-13",
-        "2016-08-15"
-      ],
-      dayNamesLength: 1, // 1 for "M", 2 for "Mo", 3 for "Mon"; 9 will show full day names. Default is 1.
-      mondayIsFirstDay: true,//set monday as first day of week. Default is false
-      eventClick: function(date) { // called before dateClick and only if clicked day has events
-        console.log(date);
-      },
-      dateClick: function(date) { // called every time a day is clicked
-        console.log(date);
-      },
-      changeMonth: function(month, year) {
-        console.log(month, year);
-      },
-      filteredEventsChange: function(filteredEvents) {
-        console.log(filteredEvents);
-      },
+  /**
+   *
+   * @param {Date} date
+   * @param {Date} dateWithTime
+   * @return {Date}
+   */
+  var setTimeByPreset = function(date, dateWithTime) {
+    var dateTime = new Date(date.toISOString());
+
+    dateTime.setUTCHours(dateWithTime.getUTCHours());
+    dateTime.setUTCMinutes(dateWithTime.getUTCMinutes());
+    dateTime.setUTCSeconds(dateWithTime.getUTCSeconds());
+    dateTime.setUTCMilliseconds(dateWithTime.getUTCMilliseconds());
+
+    return dateTime;
+  };
+
+
+  function DatesController($scope) {
+
+    var preFillData = { // TODO vstupní data z firebase
+      startTime: new Date("1970-01-01T16:00Z"),
+      duration: 120
     };
-
 
     this.durations = {
       60: '1 hodina',
+      90: '1,5 hodiny',
       120: '2 hodiny',
-      180: '3 hodiny'
+      150: '2,5 hodiny',
+      180: '3 hodiny',
+      210: '3,5 hodiny',
+      240: '4 hodiny',
+      300: '5 hodin',
+      360: '6 hodin'
+    };
+
+    this.isMultiDay = false;
+    this.duration = preFillData.duration;
+
+    this.dates = {
+      start: setTimeByPreset(getNextMonthDate(), preFillData.startTime),
+      end: null
+    };
+
+    this.calculateEndDate = function() {
+      if (this.isMultiDay) {
+        return;
+      }
+
+      var startTime = this.dates.start.getTime();
+      this.dates.end = new Date(startTime + this.duration * 60000);
+    };
+
+    this.calculateEndDate();
+
+    this.startDateOptions = {
+      defaultDate: this.dates.start,
+      dayNamesLength: 2,
+      mondayIsFirstDay: true,
+      dateClick: function(date) {
+
+        this.dates.start.setFullYear(date.year);
+        this.dates.start.setMonth(date.month);
+        this.dates.start.setDate(date.day);
+
+        this.calculateEndDate();
+
+      }.bind(this)
     };
 
 
-	}
 
-	angular.module('gugCZ.webAdmin.events.form.dates', ['flexcalendar'])
-	  .component('datesPicker', component);
+
+  }
+
+  angular.module('gugCZ.webAdmin.events.form.dates', ['flexcalendar'])
+    .component('datesPicker', component);
 
 })();
