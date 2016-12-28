db87ecddfedc5923e1b94820c8804e359068fd22
Milan Lempera <milanlempera@gmail.com>
2016-12-28 22:12:01 +0100

    links component - link type inference

--- src/app/events/form/links/links-component.js
+++ src/app/events/form/links/links-component.js
@@ -12,6 +12,29 @@
 
   function LinksController() {
 
+    this.linkTypes = [
+      {
+        type: "facebook",
+        re: /facebook.com/i
+      },
+      {
+        type: "google-plus",
+        re: /plus.google.com/i
+      },
+      {
+        type: "youtube",
+        re: /youtube.com/i
+      },
+      {
+        type: "srazy",
+        re: /srazy.info/i
+      },
+      {
+        type: "web",
+        re: /srazy.info/i
+      }
+    ];
+
     function isLinkBlank(link) {
       return link.url === "";
     }
@@ -22,7 +45,12 @@
       }
     };
 
-    this.checkEmptyItem = function() {
+    this.checkLastItem = function(index) {
+      var isLast = this.links.length - index;
+      if (!isLast) {
+        return;
+      }
+
       var lastItem = this.links[this.links.length - 1];
 
       if (lastItem.url.length > 0) {
@@ -30,8 +58,27 @@
       }
     };
 
+    this.updateType = function(index) {
+      var currentItem = this.links[index];
+
+      var passedTypes = this.linkTypes.filter(function(type) {
+        return type.re.test(currentItem.url);
+      });
+
+      if (passedTypes.length > 0) {
+        currentItem.type = passedTypes[0].type;
+      } else {
+        currentItem.type = null;
+      }
+    };
+
+    this.onLinkUpdate = function(index) {
+      this.updateType(index);
+      this.checkLastItem(index);
+    };
+
     this.$onInit = function() {
-      this.checkEmptyItem();
+      this.onLinkUpdate(0); // TODO remove or init all?
     };
 
   }
