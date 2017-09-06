c383c090ad33620f89c69500d129dcad6a2958e0
Vaclav Pavlicek <vaclav@pavlicek.cz>
2017-09-06 07:43:01 +0200

    connect venues manager to firebase

--- src/app/venue/venue.services.js
+++ src/app/venue/venue.services.js
@@ -1,29 +1,18 @@
-function firebaseFactory(firebaseDB, $q, $firebaseArray, $log, $firebaseObject) {
-  const self = this;
-
-  self.getChapterVenuesByID = function (chapterID) {
-    const chapterVenues = [];
-    $log.debug(loadVenueInfo('venue_id_1'));
-
-    // TODO try, maybe repair
-    return loadChapterVenues(chapterID).then(function (venuesKeys) {
-      Object.keys(venuesKeys).forEach(function (venueID) {
-        chapterVenues.push(loadVenueInfo(venueID));
-      });
-    });
-  };
-
-  function loadChapterVenues(chapterID) {
-    return $firebaseArray(firebaseDB.ref('chapterVenues/' + chapterID)).$loaded();
-  }
-
-  function loadVenueInfo(id) {
-    return $firebaseObject(firebaseDB.ref('venues/' + id));
-  }
-}
-
-
 angular.module('gugCZ.webAdmin.venue.services', [
   'gugCZ.firebase'
 ])
-  .service('firebaseVenues', firebaseFactory);
+  .service('firebaseVenues', firebaseVenues);
+
+function firebaseVenues(firebaseDB, $firebaseArray) {
+  this.addChapterVenueByID = function (chapterID, venueID, venue) {
+    firebaseDB.ref('chapterVenues/' + chapterID + '/').child(venueID).set(venue);
+  };
+
+  this.getChapterVenuesByID = function (chapterID) {
+    return $firebaseArray(firebaseDB.ref('chapterVenues/' + chapterID + '/'));
+  };
+
+  this.removeChapterVenueByID = function (chapterID, venueID) {
+    firebaseDB.ref('chapterVenues/' + chapterID + '/').child(venueID).remove();
+  };
+}
--- src/app/venues/venues.js
+++ src/app/venues/venues.js
@@ -20,39 +20,17 @@ angular.module('gugCZ.webAdmin.venues', [
     });
   });
 
-function VenuesController($window, $mdDialog, $log, $document) {
+function VenuesController($window, $mdDialog, firebaseVenues, $document) {
   const $ctrl = this;
 
-  this.venues = [
-    {
-      id: 1,
-      address: 'Botanická 68A, 602 00 Brno-Královo Pole',
-      name: 'MU Fakulta informatiky',
-      mapLink: 'https://www.google.cz/maps/place/Fakulta+informatiky+MU/@49.2100082,16.5966133,17z/data=!4m12!1m6!3m5!1s0x4712946af848abab:0xcbdad60137e0b956!2sFakulta+informatiky+MU!8m2!3d49.2100047!4d16.598802!3m4!1s0x4712946af848abab:0xcbdad60137e0b956!8m2!3d49.2100047!4d16.598802?hl=cs',
-      note: 'Rovnou za nosem.'
-    },
-    {
-      id: 2,
-      address: 'U Klafárku 3, 591 01 Žďár nad Sázavou',
-      name: 'Bigy Ždár nad Sázavou',
-      mapLink: 'https://www.google.cz/maps/place/Biskupsk%C3%A9+gymn%C3%A1zium/@49.5675982,15.9309421,17z/data=!3m1!4b1!4m5!3m4!1s0x470d0aaf0589cedb:0x9f237b4e5ebcb7bd!8m2!3d49.5675947!4d15.9331308?hl=cs'
-    },
-    {
-      id: 3,
-      address: 'Tolstého 1556/16, 586 01 Jihlava',
-      name: 'VŠPJ Jihlava',
-      note: 'Rovnou za nosem.'
-    },
-    {
-      id: 4,
-      address: 'nám. Čs. legií 565, 530 02 Pardubice I',
-      name: 'Univerzita Pardubice - Fakulta elektrotechniky a informatiky',
-      mapLink: 'https://www.google.cz/maps/place/Univerzita+Pardubice+-+Fakulta+elektrotechniky+a+informatiky/@50.0334344,15.7651933,17z/data=!3m1!4b1!4m5!3m4!1s0x470dccc33b38a121:0x3b1590db8ad8b477!8m2!3d50.033431!4d15.767382?hl=cs'
-    }
-  ];
+  this.$onInit = function () {
+    firebaseVenues.getChapterVenuesByID('gdg-brno').$loaded().then(data => {
+      this.venues = data.map(venue => venue);
+    });
+  };
 
   this.viewOnMap = function (venue) {
-    $window.open(venue.mapLink, '_blank');
+    $window.open(venue.mapUrl, '_blank');
   };
 
   this.deleteVenue = function (venue) {
@@ -62,6 +40,7 @@ function VenuesController($window, $mdDialog, $log, $document) {
       .ok('Ano')
       .cancel('Ne');
     $mdDialog.show(dialog).then(function() {
+      firebaseVenues.removeChapterVenueByID('gdg-brno', $ctrl.venues.indexOf(venue));
       $ctrl.venues.splice($ctrl.venues.indexOf(venue), 1);
     }, function() {});
   };
@@ -78,13 +57,8 @@ function VenuesController($window, $mdDialog, $log, $document) {
       clickOutsideToClose: true
     })
     .then(function(newVenue) {
-      if (newVenue.id) {
-        $ctrl.venues.forEach((venue) => {
-          if (newVenue.id === venue.id) {
-            newVenue = venue;
-          }
-        });
-      } else {
+      if ($ctrl.venues.indexOf(newVenue) === -1) {
+        firebaseVenues.addChapterVenueByID('gdg-brno', $ctrl.venues.length, newVenue);
         $ctrl.venues.push(newVenue);
       }
     });
