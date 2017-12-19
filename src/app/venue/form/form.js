2263043ea9012825b94ec9204f171ee237695aab
Václav Pavlíček <vaclav@pavlicek.cz>
2017-12-19 18:39:05 +0100

    add google maps places search to venues input, but has issue with changing property value

--- src/app/venue/form/form.js
+++ src/app/venue/form/form.js
@@ -1,12 +1,24 @@
+  function VenueFormController($scope, $log) {
+    this.$onInit = function () {
+      this.cityOptions = {
+        types: '(cities)',
+      };
+      this.addressOptions = {
+        types: 'address',
+      };
+    };
 
-
-  function VenueFormController() {
+    this.nameChanged = function () {
+      console.log($scope);              //vypise objekt s vlastnosti nameDetails
+      console.log($scope.nameDetails);  //Vypise hodnotu, kterou pri minule zmene vypsal predchozi log. Na zacatku to je undefined, kdyz tam zadas treba Praha, tak vypise undefined, kdyz ale napise Brno, tak dostanes Praha.
+    };
 
     this.cancel = function() {
       this.onCancel();
     };
 
     this.submit = function(form) {
+      console.log($scope.nameDetails);  //vypise hodnotu objektu nameDetails
       if (form.$invalid) {
         return;
       }
@@ -18,6 +30,7 @@
 
   angular.module('gugCZ.webAdmin.venue.form', [
     'hc.marked',
+    'ngMapAutocomplete',
     'gugCZ.webAdmin.events.form.orgs',
     'gugCZ.webAdmin.events.form.dates',
     'gugCZ.webAdmin.events.form.venue'
@@ -32,4 +45,14 @@
       },
       templateUrl: 'app/venue/form/form.html'
 
+    })
+    .directive('mapsInputChange', function() {
+      return {
+        restrict: 'A',
+        link: function ($scope, element) {
+          element.bind('change', () => {
+            $scope.$emit('GOOGLE_MAPS_AUTOCOMPLETE', {details: element.details, value: element.value});
+          });
+        }
+      };
     });
