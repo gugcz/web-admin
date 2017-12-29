e11f615045ae316eae16cbf9745c79f89f8a4e25
Václav Pavlíček <vaclav@pavlicek.cz>
2017-12-29 14:40:13 +0100

    add gps coordinates input to venue form

--- src/app/venue/form/form.js
+++ src/app/venue/form/form.js
@@ -1,10 +1,23 @@
-  function VenueFormController() {
+  function VenueFormController(venuesUtility, $document) {
     this.$onInit = function () {
-      this.venueInput = document.getElementById('venue-input');
-      this.addressInput = document.getElementById('address-input');
-      this.cityInput = document.getElementById('city-input');
-      this.urlInput = document.getElementById('url-input');
-      this.howToInput = document.getElementById('how-to-input');
+      this.venueInput = $document[0].getElementById('venue-input');
+      this.addressInput = $document[0].getElementById('address-input');
+      this.cityInput = $document[0].getElementById('city-input');
+      this.urlInput = $document[0].getElementById('url-input');
+      this.howToInput = $document[0].getElementById('how-to-input');
+      this.coords = {
+        lat: {
+          degrees: 0,
+          minutes: 0.000,
+          pole: 'N'
+        },
+        lng: {
+          degrees: 0,
+          minutes: 0.000,
+          pole: 'E'
+        }
+      };
+      this.updateCoords(this.venue.coords.lat, this.venue.coords.lng);
 
       const venueAutocomplete = new google.maps.places.Autocomplete(this.venueInput);
       const cityAutocomplete = new google.maps.places.Autocomplete(this.cityInput);
@@ -20,17 +33,29 @@
     };
 
     this.updateForm = function (place) {
+      this.venue.name = this.venueInput.value;
       this.venue.city = place.address_components.filter((address_component) => {
-        return address_component.types.indexOf("political") !== -1 && address_component.types.indexOf("administrative_area_level_2") !== -1;
+        return address_component.types.indexOf('political') !== -1 && address_component.types.indexOf('administrative_area_level_2') !== -1;
       })[0].long_name;
       this.cityInput.value = this.venue.city;
       this.venue.address = place.formatted_address;
       this.addressInput.value = this.venue.address;
       this.updateMapUrl(place);
+      this.updateCoords(place.geometry.location.lat(), place.geometry.location.lng());
+    };
+
+    this.updateCoords = function(lat, lng) {
+      if (lat && lng) {
+        this.venue.coords.lat = lat;
+        this.venue.coords.lng = lng;
+        this.coords.lat = venuesUtility.convertCoordinatesFromDecimalFormatToMinutesAndSecondsFormat(lat);
+        this.coords.lat.pole = this.coords.lat.degrees < 0 ? 'S' : 'N';
+        this.coords.lng = venuesUtility.convertCoordinatesFromDecimalFormatToMinutesAndSecondsFormat(lng);
+        this.coords.lng.pole = this.coords.lng.degrees < 0 ? 'W' : 'E';
+      }
     };
 
     this.updateMapUrl = function (place) {
-      console.log(place);
       if (place) {
         this.venue.mapUrl = place.url;
         this.urlInput.value = place.url;
@@ -46,7 +71,6 @@
       if (form.$invalid) {
         return;
       }
-
       this.onSave(this.selectedVenue);
     };
 
--- src/app/venue/venue.services.js
+++ src/app/venue/venue.services.js
@@ -1,7 +1,8 @@
 angular.module('gugCZ.webAdmin.venue.services', [
   'gugCZ.firebase'
 ])
-  .service('firebaseVenues', firebaseVenues);
+  .service('firebaseVenues', firebaseVenues)
+  .service('venuesUtility', venuesUtility);
 
 function firebaseVenues(firebaseDB, $firebaseArray) {
 
@@ -9,4 +10,13 @@ function firebaseVenues(firebaseDB, $firebaseArray) {
     return $firebaseArray(firebaseDB.ref('chapterVenues/' + chapterID + '/'));
   };
 
+}
+
+function venuesUtility() {
+  this.convertCoordinatesFromDecimalFormatToMinutesAndSecondsFormat = function (coordinates) {
+    return {
+      degrees: Math.round(coordinates - 0.5),
+      minutes: parseFloat((60 * (coordinates - Math.round(coordinates - 0.5))).toFixed(3))
+    };
+  };
 }
